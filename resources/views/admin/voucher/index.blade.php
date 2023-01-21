dc5bd54f86a4302a4b91e2b5aeca57e58bd934f9
Dmsrmdni <dimasramdani220@gmail.com>
2023-01-21 14:18:23 +0700

    menambahkan crud Voucher

--- resources/views/admin/voucher/index.blade.php
+++ resources/views/admin/voucher/index.blade.php
@@ -5,8 +5,9 @@
     <div class="card shadow-lg rounded card p-2">
         <div class="card-header" id="#atas">
             <span class="fs-4 fw-700">Data Voucher</span>
-            <a href="{{ route('voucher.create') }}" class="btn btn-sm btn-primary"><svg xmlns="http://www.w3.org/2000/svg"
-                    width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <a href="{{ route('voucher.create') }}" class="btn btn-sm btn-primary float-end"><svg
+                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg"
+                    viewBox="0 0 16 16">
                     <path fill-rule="evenodd"
                         d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                 </svg>Tambah Data</a>
