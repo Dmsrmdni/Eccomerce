4cfd14802ac8ab66390cc0d067f44f82a0a5a1ee
Dmsrmdni <dimasramdani220@gmail.com>
2023-03-02 18:54:37 +0700

    revisi

--- app/Http/Controllers/frontend/VoucherController.php
+++ app/Http/Controllers/frontend/VoucherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Voucher;
+use App\Models\VoucherUser;
 use Illuminate\Http\Request;
 
 class VoucherController extends Controller
@@ -16,6 +17,17 @@ class VoucherController extends Controller
 
     public function klaim(Request $request)
     {
-        dd($request);
+        $cek_vouchers = VoucherUser::where('user_id', auth()->user()->id)->where('voucher_id', $request->voucher_id)->first();
+
+        if (!empty($cek_vouchers)) {
+            return back()->with('gagal', 'voucher sudah di klaim');
+        } else {
+            $vouchers = new VoucherUser();
+            $vouchers->user_id = auth()->user()->id;
+            $vouchers->voucher_id = $request->voucher_id;
+            $vouchers->save();
+
+            return back()->with('berhasil', 'voucher berhasil di klaim');
+        }
     }
 }
